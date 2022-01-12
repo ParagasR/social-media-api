7fae596a48382705b1f9b06a2244e369f12d0d0a
NinjaStep <r.paragas@outlook.com>
2022-01-11 17:32:11 -0700

    set up server.js, connection to mongodb, and api routes

---
+++ server.js
@@ -0,0 +1,26 @@
+// get NPM packages
+const express = require('express');
+const morgan = require('morgan');
+
+// get the routes that will be used in the server
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// set up server variables
+const PORT = 3001;
+const app = express;
+
+// add some formatting to the terminal
+app.use(morgan('tine'));
+
+// set up formats for the express and use middleware to redirect the routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+//once the mongodb is running it will launch the server on the PORT
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`)
+  })
+})
